af1a1d3f4eab1a983936135db3fa9347c6b2f521
Pedro Bernardes <pedroabernardes11@gmail.com>
2025-03-12 21:07:19 -0300

    refactor: prisma model

---
+++ prisma/migrations/20250313000709_remove_unused_property/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `hasReceivedTicket` on the `payments` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "payments" DROP COLUMN "hasReceivedTicket";
