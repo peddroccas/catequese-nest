4c94de366a91b1f029589da415c015ec9b9ba5d0
Pedro Antunes <pedroabernardes11@gmail.com>
2025-03-21 12:27:02 -0300

    feat: import modules

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env/env'
+import { AuthModule } from './auth/auth.module'
+import { DatabaseModule } from './database/database.module'
+import { CryptographyModule } from './cryptography/cryptography.module'
 
 @Module({
   imports: [
@@ -8,6 +11,9 @@ import { envSchema } from './env/env'
       validate: env => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
+    DatabaseModule,
+    CryptographyModule,
   ],
   controllers: [],
   providers: [],
