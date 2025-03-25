1bee8dbb042cfe4bc5b14d33499b082a94df9fcb
Pedro Antunes <pedroabernardes11@gmail.com>
2025-03-25 11:15:21 -0300

    refactor: salt length

--- src/infra/cryptography/bcrypt-hasher.ts
+++ src/infra/cryptography/bcrypt-hasher.ts
@@ -3,7 +3,7 @@ import { HashGenerator } from '@/domain/catechists/cryptography/hash-generator'
 import { hash, compare } from 'bcryptjs'
 
 export class BcryptHasher implements HashGenerator, HashComparer {
-  private HASH_SALT_LENGTH = 8
+  private HASH_SALT_LENGTH = 6
 
   hash(plain: string): Promise<string> {
     return hash(plain, this.HASH_SALT_LENGTH)
