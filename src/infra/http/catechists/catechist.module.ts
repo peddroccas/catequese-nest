d92b62656c55bf1f273fdcb29c1442516ad5ec40
Pedro Antunes <pedroabernardes11@gmail.com>
2025-03-25 11:59:18 -0300

    feat: auth and delete

--- src/infra/http/catechists/catechist.module.ts
+++ src/infra/http/catechists/catechist.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common'
 import { CatechistController } from './controllers/catechist.controller'
 import { CreateCatechistUseCase } from '@/domain/catechists/use-cases/create-catechist.use-case'
+import { AuthenticateCatechistUseCase } from '@/domain/catechists/use-cases/authenticate-catechist.use-case'
+import { DeleteCatechistUseCase } from '@/domain/catechists/use-cases/delete-catechist.use-case'
 
 @Module({
   controllers: [CatechistController],
-  providers: [CreateCatechistUseCase],
+  providers: [
+    CreateCatechistUseCase,
+    AuthenticateCatechistUseCase,
+    DeleteCatechistUseCase,
+  ],
 })
 export class CatechistModule {}
--- src/infra/http/catechists/controllers/catechist.controller.ts
+++ src/infra/http/catechists/controllers/catechist.controller.ts
@@ -1,17 +1,31 @@
+import { AuthenticateCatechistRequestDto } from '@/domain/catechists/dtos/request/authenticate-catechist.dto'
 import { CreateCatechistRequestDto } from '@/domain/catechists/dtos/request/create-catechist.dto'
+import { AuthenticateCatechistResponseDto } from '@/domain/catechists/dtos/response/authenticate-catechist.dto'
+import { Role } from '@/domain/catechists/enums/role'
+import { AuthenticateCatechistUseCase } from '@/domain/catechists/use-cases/authenticate-catechist.use-case'
 import { CreateCatechistUseCase } from '@/domain/catechists/use-cases/create-catechist.use-case'
+import { DeleteCatechistUseCase } from '@/domain/catechists/use-cases/delete-catechist.use-case'
 import { Public } from '@/infra/auth/public'
+import { Roles } from '@/infra/auth/roles.decorator'
 import {
   Body,
   ConflictException,
   Controller,
+  Delete,
   HttpCode,
+  NotFoundException,
+  Param,
   Post,
+  UnauthorizedException,
 } from '@nestjs/common'
 
 @Controller('/catechists')
 export class CatechistController {
-  constructor(private createCatechist: CreateCatechistUseCase) {}
+  constructor(
+    private createCatechist: CreateCatechistUseCase,
+    private authenticateCatechist: AuthenticateCatechistUseCase,
+    private deleteCatechist: DeleteCatechistUseCase
+  ) {}
 
   @Post()
   @Public()
@@ -21,6 +35,32 @@ export class CatechistController {
 
     if (result.isLeft()) {
       throw new ConflictException()
+    }
+  }
+
+  @Post('/auth')
+  @Public()
+  async authenticate(
+    @Body() authenticateCatechistRequestDto: AuthenticateCatechistRequestDto
+  ) {
+    const response = await this.authenticateCatechist.execute(
+      authenticateCatechistRequestDto
+    )
+
+    if (response.isLeft()) {
+      throw new UnauthorizedException()
+    }
+
+    return response.value
+  }
+
+  @Delete('/:id')
+  @Roles([Role.COORDINATOR])
+  async delete(@Param('id') id: string) {
+    const response = await this.deleteCatechist.execute(id)
+
+    if (response.isLeft()) {
+      throw new NotFoundException()
     }
   }
 }
